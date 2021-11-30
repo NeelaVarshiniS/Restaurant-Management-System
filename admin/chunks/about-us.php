5ce4cdfe618a71bb15eed83d058e5b7dc7185025
NeelaVarshiniS <74665048+NeelaVarshiniS@users.noreply.github.com>
2021-11-30 19:32:08 +0530

    Delete admin directory

--- admin/chunks/about-us.php
+++
@@ -1,8 +0,0 @@
-<section class="fabout">
-		<div class="section white center">
-		      <div class="row container">
-		        <h2 class="header">ABOUT US</h2>
-		        <p class="grey-text text-darken-3 lighten-3">A restaurant in Tirupur is going all-out for the 15th anniversary of the debut of Mean Girls! The establishment, Twisted, has partnered with Uber Eats to launch a “Pink Wednesday” menu in honor of the iconic film, which premiered on April 30, 2004, and the themed food items are sure to bring joy to anyone who is still trying to make “fetch” happen.
-Unfortunately for those living in Tirupur, there are no current plans to bring this menu stateside, as Twisted is the world’s first delivery-only restaurant based in the English city. </p>
-		      </div>
-	</section>
