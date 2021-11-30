09cc737b8cfe6281adb80eb45ed33afada9105bf
NeelaVarshiniS <74665048+NeelaVarshiniS@users.noreply.github.com>
2021-11-30 19:33:43 +0530

    Add files via upload

---
+++ chunks/description.php
@@ -0,0 +1,14 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Resturant Powered By Girls</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		        	<p>A restaurant in Tirupur is going all-out for the 15th anniversary of the debut of Mean Girls! The establishment, Twisted, has partnered with Uber Eats to launch a “Pink Wednesday” menu in honor of the iconic film, which premiered on April 30, 2004, and the themed food items are sure to bring joy to anyone who is still trying to make “fetch” happen.
+Unfortunately for those living in Tirupur, there are no current plans to bring this menu stateside, as Twisted is the world’s first delivery-only restaurant based in the English city.</p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
